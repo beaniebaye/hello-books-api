fe2d66d907de94445fa905d00f37eab2fb52a78d
bailey <baileyholderman@gmail.com>
2021-10-16 11:58:53 -0700

    fixes broken endpoint

--- app/routes.py
+++ app/routes.py
@@ -14,3 +14,14 @@ def hello_world_json():
         "message" : "Let's get this bread",
         "hobbies" : ["Weightlifting", "Animal Crossing :)", "Roller skating"]
     }, 200
+
+@hello_world_bp.route("/broken-endpoint-with-broken-server-code")
+def broken_endpoint():
+    response_body = {
+        "name": "Ada Lovelace",
+        "message": "Hello!",
+        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
+    }
+    new_hobby = "Surfing"
+    response_body["hobbies"].append(new_hobby)
+    return response_body
