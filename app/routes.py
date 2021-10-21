df9dba1be37c9c3af3e02b8b5775a7711c136abb
bailey <baileyholderman@gmail.com>
2021-10-20 19:00:35 -0700

    Creates book model and creates post endpoint

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, jsonify
+from app import db
+from app.models.book import Book
+from flask import Blueprint, jsonify, make_response, request
 
 
 
@@ -6,6 +8,18 @@ from flask import Blueprint, jsonify
 
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
+@books_bp.route("", methods=["POST"])
+def handle_books():
+    request_body = request.get_json()
+    new_book = Book(
+        title=request_body["title"],
+        description = request_body["description"]
+    )
+
+    db.session.add(new_book)
+    db.session.commit()
+
+    return make_response(f"Book {new_book.title} successfully created", 201)
 
 #Uses our books blueprint
 # @books_bp.route("", methods = ["GET"])
