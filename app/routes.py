2dd5be6dc2c035a32f05a9c4de812fe37a3495d9
bailey <baileyholderman@gmail.com>
2021-10-20 19:47:39 -0700

    adds refactored read all books endpoint

--- app/routes.py
+++ app/routes.py
@@ -8,18 +8,40 @@ from flask import Blueprint, jsonify, make_response, request
 
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
-@books_bp.route("", methods=["POST"])
+@books_bp.route("", methods=["GET","POST"])
 def handle_books():
-    request_body = request.get_json()
-    new_book = Book(
-        title=request_body["title"],
-        description = request_body["description"]
-    )
 
-    db.session.add(new_book)
-    db.session.commit()
+    if request.method == "GET":
 
-    return make_response(f"Book {new_book.title} successfully created", 201)
+        # books = Book.query.all()
+        # books_response = []
+        # for book in books:
+        #     books_response.append({
+        #         "id": book.id,
+        #         "title": book.title,
+        #         "description": book.description
+        #     })
+        # return jsonify(books_response)
+
+        books = Book.query.all()
+
+        return jsonify([vars(book) for book in books])
+
+    elif request.method == "POST":
+        request_body = request.get_json()
+
+        if "title" not in request_body or "description" not in request_body:
+            return make_response("Invalid request", 400)
+
+        new_book = Book(
+            title=request_body["title"],
+            description = request_body["description"]
+        )
+
+        db.session.add(new_book)
+        db.session.commit()
+
+        return make_response(f"Book {new_book.title} successfully created", 201)
 
 #Uses our books blueprint
 # @books_bp.route("", methods = ["GET"])
